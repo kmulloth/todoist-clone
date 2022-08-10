108c008ee8182559cec1a722fd399a8ad51a8497
KMulloth <k.mulloth@gmail.com>
2022-08-10 18:28:50 -0500

    sections select functional

--- react-app/src/components/EachProject/index.js
+++ react-app/src/components/EachProject/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import {useParams} from 'react-router-dom';
 import { getTasks } from "../../store/tasks";
 import { getProjects } from "../../store/projects";
+import { getSections } from "../../store/sections";
 import DeleteProjectModal from "../DeleteProjectModal";
 import EditProjectModal from "../EditProjectModal";
 import EditTaskModal from "../EditTaskModal";
@@ -12,18 +13,22 @@ import AddSection from "../AddSection";
 function Project() {
 
     const projects = useSelector(state => state.projects);
+    const sections = useSelector(state => state.sections);
     const tasks = useSelector(state => state.tasks);
-    console.log('Tasks: ', Object.values(tasks));
     const {projectId} = useParams();
     const project = projects[projectId];
     const projectTasks = Object.values(tasks).filter(task => task.project_id == projectId);
+    const projectSections = Object.values(sections).filter(section => section.project_id == projectId);
     const dispatch = useDispatch();
+
+    console.log('Sections: ', projectSections, 'Tasks: ', projectTasks);
 
     const [showAddSection, setShowAddSection] = useState(false);
 
     useEffect(() => {
         dispatch(getTasks())
         dispatch(getProjects())
+        dispatch(getSections())
     }, [dispatch]);
 
     return (
@@ -39,12 +44,35 @@ function Project() {
                 {showAddSection && <AddSection projectId={projectId} setShowAddSection={setShowAddSection}/>}
             </div>
             <div className="project-tasks">
-                {projectTasks.map(task => (
-                    <div key={task.id} className="task">
-                        <h2>{task.name}</h2>
-                        <p>{task.description}</p>
-                        <EditTaskModal task={task}  />
-                        <DeleteTaskModal task={task} />
+                {projectTasks.map(task => {
+                    if (task.section_id == null) {
+                        return (
+                            <div key={task.id} className="task">
+                                <h3>{task.name}</h3>
+                                <p>{task.description}</p>
+                                <EditTaskModal task={task}/>
+                                <DeleteTaskModal task={task}/>
+                            </div>
+                        )
+                    }
+                })}
+            </div>
+            <div className="project-sections">
+                {projectSections.map(section => (
+                    <div className='section'>
+                        <h4>{section.name}</h4>
+                        {projectTasks.map(task => {
+                            if (task.section_id == section.id) {
+                                return (
+                                    <div key={task.id} className="task">
+                                        <h3>{task.name}</h3>
+                                        <p>{task.description}</p>
+                                        <EditTaskModal task={task}/>
+                                        <DeleteTaskModal task={task}/>
+                                    </div>
+                                )
+                            }
+                        })}
                     </div>
                 ))}
             </div>
--- react-app/src/components/EditTaskModal/EditTask.js
+++ react-app/src/components/EditTaskModal/EditTask.js
@@ -10,11 +10,22 @@ function EditTask({task, setShowModal}) {
     const [due, setDue] = useState(task?.due)
     const [priority, setPriority] = useState(task?.priority)
     const [projectId, setProjectId] = useState(task?.project_id)
+    const [sectionId, setSectionId] = useState(task?.section_id)
 
     const currentUser = useSelector(state => state.session.user)
     const projects = useSelector(state => state.projects)
+    const sections = useSelector(state => state.sections)
     const dispatch = useDispatch()
     const history = useHistory()
+
+    const userProjects = Object.values(projects).map(project => {
+        if (project.user_id == currentUser.id){
+            const sectionsArr = Object.values(sections).filter(section => section.project_id == project.id)
+            return {id: project.id, name: project.name, sections: sectionsArr}
+        }
+    })
+
+    console.log('User Projects:', userProjects)
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -24,7 +35,7 @@ function EditTask({task, setShowModal}) {
             name,
             description,
             // complete: false,
-            // section_id: task.section_id,
+            section_id: sectionId,
             project_id: projectId,
             due,
             priority,
@@ -35,6 +46,15 @@ function EditTask({task, setShowModal}) {
             dispatch(getTasks())
             setShowModal(false)
         })
+    }
+
+    const handleSectionSelect = (e) => {
+
+        const projectConfig = e.target.value.split(',')
+        console.log('Project:', projectConfig[0], 'incoming Section:', projectConfig[1],'old section:', sectionId)
+        setProjectId(projectConfig[0])
+        setSectionId(projectConfig[1])
+        // console.log(projectId, sectionId)
     }
 
     return (
@@ -60,10 +80,19 @@ function EditTask({task, setShowModal}) {
             </div>
             <div className='form-group'>
                 <label htmlFor='project'>Project</label>
-                <select className='form-control' id='project' value={projectId} onChange={(e) => setProjectId(e.target.value)}>
-                    <option value={null}>None</option>
-                    {Object.values(projects).map(project => (
-                        <option value={project.id}>{project.name}</option>
+                <select
+                className='form-control'
+                id='project'
+                value={[projectId, sectionId]}
+                onChange={(e) => handleSectionSelect(e)}>
+                    <option value={[null, null]}>None</option>
+                    {userProjects.map(project => (
+                        <>
+                        <option key={project.id} value={[project.id, null]}>{project.name}</option>
+                        {project.sections.length > 0 && project.sections.map(section => (
+                            <option value={[project.id, section.id]}>{section.name}</option>
+                        ))}
+                        </>
                     ))}
                 </select>
             </div>
